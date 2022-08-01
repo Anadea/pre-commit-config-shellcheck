a2c554c52c330f74d98d66672e46133b2ce70392
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2022-08-01 16:47:22 +0300

    First revision of composite github action

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -eau pipefail; \
-
-INPUT_CONFIG="${INPUT_CONFIG:-.pre-commit-config.yaml}"; \
-export INPUT_CONFIG; \
-
-/opt/.env/bin/pre_commit_config_shellcheck.py "${INPUT_CONFIG}"
